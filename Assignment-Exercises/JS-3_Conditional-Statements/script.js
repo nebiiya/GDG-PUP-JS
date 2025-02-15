a9d8456c24b01e75c7dcba713b10cd09050e1907
nebiiya <phoebemailsu@gmail.com>
2025-02-15 16:18:32 +0800

    update script.js

--- Assignment-Exercises/JS-3_Conditional-Statements/script.js
+++ Assignment-Exercises/JS-3_Conditional-Statements/script.js
@@ -1,3 +1,17 @@
-function checkAge() {
+function checkAge(){
     // write your code here 
+    var value = document.getElementById('ageInput').value;
+    var age = Number(value);
+    let category
+    if(value >= 0 && value <= 12){
+        category = "Child";
+    } else if(value >= 13 && value <= 19){
+        category ="Teenager";
+    } else if(value >= 20){
+        category = "Adult";
+    } else {
+        category ="Invalid age. Please enter a valid number";
+    }
+
+    document.getElementById('result').innerText=category;
 }
